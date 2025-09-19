2cd70aaf472afd82d5976f8da01a817c397d3834
Plo4i <midag@abv.bg>
2025-09-19 15:04:47 +0200

    feat: implement courses feature with API integration and UI components

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,4 +7,11 @@ export default defineSchema({
 		name: v.string(),
 		clerkId: v.string(),
 	}).index('by_ClerkId', ['clerkId']),
+
+	courses: defineTable({
+		title: v.string(),
+		description: v.string(),
+		imageUrl: v.string(),
+		price: v.number(),
+	}),
 });
